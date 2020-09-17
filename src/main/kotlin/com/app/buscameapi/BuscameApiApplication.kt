8da1e68bfa85e9f07f14ba9cbc328c21a6d3758b
Gabriel Lima <bielsantoslima10@gmail.com>
2020-09-17 10:13:56 -0400

    [#1] Create project spring

---
+++ src/main/kotlin/com/app/buscameapi/BuscameApiApplication.kt
@@ -0,0 +1,11 @@
+package com.app.buscameapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BuscameApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<BuscameApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/app/buscameapi/BuscameApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.app.buscameapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BuscameApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
